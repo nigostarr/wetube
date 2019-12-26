cca8802a6c45efc9f3ce31627c0c8cb8367eb14f
Chang Hyun Ji <nigostarrgit.github.com>
2019-12-26 21:24:37 +0900

    #1.3 Before Log In HTML

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,27 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+import { request } from "express";
+
+export const home = (req, res) =>
+  res.render("home", { pageTitle: "Home", fuck: 1234 });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("Search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) => {
+  res.render("Videos", { pageTitle: "Videos" });
+};
+export const upload = (req, res) => {
+  res.render("Upload", { pageTitle: "Upload" });
+};
+export const videoDetail = (req, res) => {
+  res.render("VideoDetail", { pageTitle: "Video Detail" });
+};
+export const editVideo = (req, res) => {
+  res.render("EditVideo", { pageTitle: "Edit Video" });
+};
+export const deleteVideo = (req, res) => {
+  res.render("DeleteVideo", { pageTitle: "Delete Video" });
+};
