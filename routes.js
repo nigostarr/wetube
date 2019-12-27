d973beaa0054a7bc71f7bc590e21fa0a55952985
Ji Chang Hyun <jichanghyun@jichanghyeon-ui-MacBookPro.local>
2019-12-27 15:50:41 +0900

    #1.5 Before MongoDB

--- routes.js
+++ routes.js
@@ -1,20 +1,18 @@
-//Global
-
+// Global
 const HOME = "/";
 const JOIN = "/join";
 const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
-//
 
-// User
+// Users
 
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
-//videos
+// Videos
 
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
@@ -29,12 +27,24 @@ const routes = {
   logout: LOGOUT,
   search: SEARCH,
   users: USERS,
-  userDetail: USER_DETAIL,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
   videos: VIDEOS,
   upload: UPLOAD,
-  videoDetail: VIDEO_DETAIL,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
   editVideo: EDIT_VIDEO,
   deleteVideo: DELETE_VIDEO
 };
